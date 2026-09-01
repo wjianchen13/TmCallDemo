778d8eaadcf3b818ee2cc091205ae7f94a047747
chenwj <1812821235@qq.com>
2026-09-01 10:43:00 +0800

    需要自己的信令实现，所以不能使用TuiCallKit

--- tuicallkit-kt/src/main/java/com/tencent/qcloud/tuikit/tuicallkit/view/component/inviteuser/GroupMemberInfo.kt
+++
@@ -1,8 +0,0 @@
-package com.tencent.qcloud.tuikit.tuicallkit.view.component.inviteuser
-
-class GroupMemberInfo {
-    var userId: String? = ""
-    var userName: String? = ""
-    var avatar: String? = ""
-    var isSelected: Boolean = false
-}
